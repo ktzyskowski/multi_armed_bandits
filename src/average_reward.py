975abcabbd5d57a68b5eaf84590a18f01b5fa99f
Kevin Zyskowski <ktzyskowski@gmail.com>
2025-10-05 10:56:19 -0400

    Move factory functions to Testbed class

--- src/average_reward.py
+++ src/average_reward.py
@@ -1,16 +1,9 @@
-import functools
 from multiprocessing import Pool
 
 import numpy as np
 import plotly.graph_objects as go
 
-from mab.bandit.nonstationary_bandit import NonstationaryMultiArmedBandit
-from mab.bandit.stationary_bandit import MultiArmedBandit
-from mab.policy.egreedy_policy import EpsilonGreedyPolicy
-from mab.policy.gradient_policy import GradientPolicy
-from mab.policy.random_policy import RandomPolicy
-from mab.policy.ucb_policy import UpperConfidenceBoundPolicy
-from mab.testbed import Testbed
+from mab.testbed import Testbed, TestbedConfig
 
 N_ARMS = 10
 N_RUNS = 2_000
@@ -18,86 +11,49 @@ N_STEPS = 1_000
 N_PROCESSES = 4
 
 
-def make_random_policy(random_seed: int):
-    return RandomPolicy(k=N_ARMS, random_seed=None)
-
-
-def make_epsilon_greedy_policy(random_seed: int, eps: float):
-    return EpsilonGreedyPolicy(eps=eps, k=N_ARMS, random_seed=None)
-
-
-def make_ucb_policy(random_seed: int, c: float):
-    return UpperConfidenceBoundPolicy(c=c, k=N_ARMS, random_seed=None)
-
-
-def make_gradient_policy(random_seed: int, alpha: float, baseline: bool):
-    return GradientPolicy(alpha=alpha, baseline=baseline, k=N_ARMS, random_seed=None)
-
-
-def run(testbed_config: dict):
-    if testbed_config.get("nonstationary"):
-        bandit_factory = lambda random_seed: NonstationaryMultiArmedBandit(
-            k=N_ARMS,
-            random_seed=None,
-        )
-    else:
-        bandit_factory = lambda random_seed: MultiArmedBandit(
-            k=N_ARMS,
-            random_seed=None,
-        )
-
-    testbed = Testbed(
-        policy_factory=testbed_config.get("policy_factory"),
-        bandit_factory=bandit_factory,
-    )
-    rewards = testbed.run(N_RUNS, N_STEPS)
-    return rewards
-
-
 def main():
-    testbed_configs = [
-        # {
-        #     "nonstationary": False,
-        #     "name": "eps_greedy (eps=0.1)",
-        #     "policy_factory": functools.partial(make_epsilon_greedy_policy, eps=0.1),
-        # },
-        # {
-        #     "nonstationary": False,
-        #     "name": "ucb (c=2)",
-        #     "policy_factory": functools.partial(make_ucb_policy, c=2),
-        # },
+    experiments: list[TestbedConfig] = [
         {
-            "nonstationary": False,
+            "name": "eps_greedy (eps=0.1)",
+            "type": "eps_greedy",
+            "stationary": True,
+            "args": {"eps": 0.1},
+        },
+        {
+            "name": "ucb (c=2)",
+            "type": "ucb",
+            "stationary": True,
+            "args": {"c": 2.0},
+        },
+        {
             "name": "gradient (alpha=0.1)",
-            "policy_factory": functools.partial(
-                make_gradient_policy, alpha=0.1, baseline=True
-            ),
+            "type": "gradient",
+            "stationary": True,
+            "args": {"alpha": 0.1},
         },
         {
-            "nonstationary": False,
             "name": "gradient (alpha=0.4)",
-            "policy_factory": functools.partial(
-                make_gradient_policy, alpha=0.4, baseline=True
-            ),
+            "type": "gradient",
+            "stationary": True,
+            "args": {"alpha": 0.4},
         },
         {
-            "nonstationary": False,
             "name": "gradient (alpha=0.1, no-baseline)",
-            "policy_factory": functools.partial(
-                make_gradient_policy, alpha=0.1, baseline=False
-            ),
+            "type": "gradient",
+            "stationary": True,
+            "args": {"alpha": 0.1, "baseline": True},
         },
         {
-            "nonstationary": False,
             "name": "gradient (alpha=0.4, no-baseline)",
-            "policy_factory": functools.partial(
-                make_gradient_policy, alpha=0.4, baseline=False
-            ),
+            "type": "gradient",
+            "stationary": True,
+            "args": {"alpha": 0.4, "baseline": True},
         },
     ]
 
+    testbed = Testbed(k=N_ARMS, n_runs=N_RUNS, n_steps=N_STEPS)
     with Pool(processes=N_PROCESSES) as pool:
-        results = pool.map(run, testbed_configs)
+        results = pool.map(testbed.run, experiments)
 
     # concatenate rewards from all experiments in 0th dim:
     # new dims = [n_experiments, n_runs, n_steps]
@@ -109,12 +65,12 @@ def main():
     fig = go.Figure()
     fig.update_layout(xaxis_title="Step", yaxis_title="Average Reward")
     fig.add_hline(y=0.0, line_dash="dash")
-    for i in range(len(testbed_configs)):
+    for i in range(len(experiments)):
         fig.add_trace(
             go.Scatter(
                 y=average_rewards[i],
                 mode="lines",
-                name=testbed_configs[i].get("name"),
+                name=experiments[i].get("name"),
             )
         )
     fig.show()
--- src/mab/testbed.py
+++ src/mab/testbed.py
@@ -1,36 +1,80 @@
-from typing import Callable
+from typing import Optional, TypedDict
 
 import numpy as np
 import tqdm
 
+from mab.bandit.nonstationary_bandit import NonstationaryMultiArmedBandit
 from mab.bandit.stationary_bandit import MultiArmedBandit
 from mab.policy import Policy
+from mab.policy.egreedy_policy import EpsilonGreedyPolicy
+from mab.policy.gradient_policy import GradientPolicy
+from mab.policy.random_policy import RandomPolicy
+from mab.policy.ucb_policy import UpperConfidenceBoundPolicy
+
+
+class TestbedConfig(TypedDict):
+    name: str
+    type: str
+    stationary: bool
+    args: Optional[dict[str, any]]
 
 
 class Testbed:
     def __init__(
         self,
-        policy_factory: Callable[[int], Policy],
-        bandit_factory: Callable[[int], MultiArmedBandit],
+        k: int,
+        n_runs: int,
+        n_steps: int,
     ):
-        self._policy_factory = policy_factory
-        self._bandit_factory = bandit_factory
+        self._k = k
+        self._n_runs = n_runs
+        self._n_steps = n_steps
 
-    def run(self, n_runs: int, n_steps: int, verbose: bool = False):
-        rewards = np.zeros((n_runs, n_steps), dtype=np.float32)
+    def policy_factory(
+        self,
+        config: TestbedConfig,
+        random_seed: int = None,
+    ) -> Policy:
+        if config.get("type") == "random":
+            return RandomPolicy(k=self._k, random_seed=random_seed)
+        elif config.get("type") == "eps_greedy":
+            return EpsilonGreedyPolicy(
+                k=self._k, random_seed=random_seed, **config.get("args")
+            )
+        elif config.get("type") == "ucb":
+            return UpperConfidenceBoundPolicy(
+                k=self._k, random_seed=random_seed, **config.get("args")
+            )
+        elif config.get("type") == "gradient":
+            return GradientPolicy(
+                k=self._k, random_seed=random_seed, **config.get("args")
+            )
+
+    def bandit_factory(
+        self,
+        config: TestbedConfig,
+        random_seed: int = None,
+    ) -> MultiArmedBandit:
+        if config.get("stationary"):
+            return MultiArmedBandit(k=self._k, random_seed=random_seed)
+        else:
+            return NonstationaryMultiArmedBandit(k=self._k, random_seed=random_seed)
+
+    def run(self, config: TestbedConfig, verbose: bool = False):
+        rewards = np.zeros((self._n_runs, self._n_steps), dtype=np.float32)
 
         iterator = (
-            tqdm.trange(n_runs, desc=f"Simulating {n_runs:,} bandit runs")
+            tqdm.trange(self._n_runs, desc=f"Simulating bandit runs...")
             if verbose
-            else range(n_runs)
+            else range(self._n_runs)
         )
 
         for run_idx in iterator:
             policy_random_seed = run_idx
-            policy = self._policy_factory(random_seed=policy_random_seed)
-            bandit_random_seed = run_idx + n_runs + n_steps
-            bandit = self._bandit_factory(random_seed=bandit_random_seed)
-            for step_idx in range(n_steps):
+            policy = self.policy_factory(config, random_seed=policy_random_seed)
+            bandit_random_seed = run_idx + self._n_runs + self._n_steps
+            bandit = self.bandit_factory(config, random_seed=bandit_random_seed)
+            for step_idx in range(self._n_steps):
                 action = policy()
                 reward = bandit.step(action)
                 policy.update(action, reward)
